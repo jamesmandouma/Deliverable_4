4c3e0aad162b89e49ed15837639ea93e2c8eb4c4
james <james@DESKTOP-OQN1M2V>
2023-02-09 04:07:43 -0500

    Add project files.

---
+++ Deliverable_4/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Programmer: James Mandouma
+ * Description: C# Console App to compute first 25 numbers of fibonacci sequence.
+ * 
+ * Date: 2/9/2023 */ 
+
+using System;
+
+namespace Deliverable_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create fibonacci array, initial values
+            int[] fibonacci = new int[25];
+            fibonacci[0] = 0;
+            fibonacci[1] = 1;
+
+            // Loop through remaining values
+            for (int i = 2; i < 25; i++)
+            {
+                fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
+            }
+            
+            for (int i = 0; i < 25; i++)
+            {
+                Console.WriteLine("Fibonacci number (" + i + ") = " + fibonacci[i]);
+            }
+        }
+    }
+}
